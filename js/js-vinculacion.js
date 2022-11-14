cfcab89b565dc36aef467a00e13d2ac6c672c1b7
nuskacg <rodelugaliza@gmail.com>
2022-11-14 17:13:51 -0300

    Curso-Hack-Academy

---
+++ js/js-vinculacion.js
@@ -0,0 +1,17 @@
+// comentario de una línea
+/* comentario de 
+varias líneas*/
+
+var fullName = "Ana Lorena";
+var age = 18;
+
+//el console log sirve para imprimir cosas en la consola del navegador.
+//console.log(fullName);
+//console.log(age);
+//console.log(fullName + " " + age);
+
+var gretting = "Hola mi nombre es " + fullName + " " + "y mi edad es " + age;
+
+console.log(gretting);
+
+
